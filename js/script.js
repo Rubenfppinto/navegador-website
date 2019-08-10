b9692c6436280144e70c26aef7b38ffc05f02dd3
Rubenfppinto <rpinto@spartaglobal.com>
2019-08-10 16:33:57 +0100

    finished sections for production

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.modal').modal();
     $('.materialboxed').materialbox();
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
 });
